93c2ecc9e53957fbe8c50230fab6351e65b9537f
Bryan Havenstein <bhavenst@redhat.com>
2021-12-14 15:13:50 -0700

    Add new django based billing code

--- azure_billing/cmd.py
+++ azure_billing/cmd.py
@@ -1,52 +1,45 @@
-#!/opt/app-root/bin/python3
+#!/usr/bin/python3
 
 import argparse
-import sys
-import yaml
+import azure
+import django
+import logging
+import os
 
-from . import billing
-from . import db
+logger = logging.getLogger()
+
+DIM = 'hosts'
 
 
 def processArgs():
-  parser = argparse.ArgumentParser(description='Ansible Automation Platform Azure billing connector', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  group = parser.add_mutually_exclusive_group()
-  group.add_argument('-it', action='store_true', dest='doinstalltrigger', help='Install unique host trigger and process billing')
-  group.add_argument('-rt', action='store_true', dest='doremovetrigger', help='Remove unique host trigger and stop')
-  return parser.parse_args()
+    parser = argparse.ArgumentParser(description='Ansible Automation Platform Azure billing connector', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-d', action='store_true', dest='debugmode', help="Enable debug output")
+    return parser.parse_args()
 
-def loadConfig():
-  with open("config.yaml") as cfgfile:
-    cfg = yaml.safe_load(cfgfile)
-  return cfg
-
+# Main
 args = processArgs()
-cfg = loadConfig()
+logging.basicConfig(level=logging.DEBUG if args.debugmode else logging.INFO, format='%(levelname)s %(asctime)s\t%(message)s')
 
-# Connect to DB
-db.login(cfg['db']['name'],
-         cfg['db']['user'],
-         cfg['db']['pass'],
-         cfg['db']['host'],
-         cfg['db']['port'])
+# Bootstrap django (orm only)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+django.setup()
+import db # noqa: E402 - Must follow django setup
 
+logger.info("Checking for unbilled hosts.")
+unbilled = db.getUnbilledHosts()
 
-# Add or remove trigger if requested
-if args.doinstalltrigger:
-  db.installHostHistoryTrigger()
-  print("Trigger installed.")
-elif args.doremovetrigger:
-  db.removeHostHistoryTrigger()
-  print("Trigger removed.")
-  sys.exit(0)
+if unbilled:
+    logger.info("%d unbilled hosts found, sending billing data to Azure" % len(unbilled))
 
+    billing_record = azure.pegBillingCounter(DIM, len(unbilled))
+    billing_record['hosts'] = unbilled
+    billing_record['dimension'] = DIM
 
-# Do billing for unbilled hosts
-hosts = db.getNewHosts()
+    # Record billing data
+    db.recordBillingInstance(billing_record)
 
-
-if hosts:
-  billing.pegBillingCounter('someplan', 'hosts', len(hosts))
-
-# Mark hosts as billed if successful
-db.markHostsBilled(hosts)
+    # Mark hosts as billed if successful
+    logger.info("Marking hosts as billed/recorded.")
+    db.markHostsBilled(unbilled)
+else:
+    logger.info("No unbilled hosts found.")
--- azure_billing/db.py
+++ azure_billing/db.py
@@ -1,60 +1,82 @@
-#!/usr/bin/python3
+from django.db import models
 
-import psycopg2
+from main.models import JobHostSummary
+from billing.models import BilledHost
+from billing.models import BillingRecord
 
-conn = None
-curs = None
+import logging
 
-createReplaceTrigger = """CREATE OR REPLACE FUNCTION record_unique_host() RETURNS TRIGGER AS $record_host$
-                BEGIN
-                    --
-                    -- Copy host_name and date from main_jobhostsummary to unique_host
-                    -- if not already present
-                    --
-                    IF (TG_OP = 'INSERT') THEN
-                        INSERT INTO unique_host (host_name, executed)
-                        VALUES (NEW.host_name, NEW.created)
-                        ON CONFLICT DO NOTHING;
-                    END IF;
-                    RETURN NULL;
-                END;
-            $record_host$ LANGUAGE plpgsql;
+logger = logging.getLogger()
 
-            CREATE TRIGGER record_host
-            AFTER INSERT OR UPDATE ON main_jobhostsummary
-                FOR EACH ROW EXECUTE FUNCTION record_unique_host();"""
 
-# Drops both the function and the trigger
-dropTrigger = """DROP FUNCTION record_unique_host CASCADE;"""
+class BillingRouter:
+    """
+    Routes db queries to proper database
+    """
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label == 'main':
+            return 'awx'
+        else:
+            return 'default'
 
-def installHostHistoryTrigger():
-    getCursor().execute(createReplaceTrigger)
-    conn.commit()
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'billing':
+            return 'default'
+        return None
 
-def removeHostHistoryTrigger():
-    getCursor().execute(dropTrigger)
-    conn.commit()
+    def allow_relation(self, obj1, obj2, **hints):
+        # If multiple tables added to billing db, update this to allow
+        # relationships between tables in that db.
+        return False
 
-def login(dbName, dbUser, dbPass, dbHost, dbPort):
-    global conn
-    conn = psycopg2.connect(database=dbName, user=dbUser, password = dbPass, host=dbHost, port=dbPort)
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        # Allow migrations only for the default database
+        if db == 'default':
+            return app_label == 'billing'
+        return None
 
-def getCursor():
-    global curs
-    if conn is None:
-        print("Please log into DB!")
-    elif curs is None:
-        curs = conn.cursor()
-    return curs
 
-def getNewHosts():
-    getCursor().execute("SELECT * FROM unique_host WHERE billed_date IS NULL")
-    hosts = getCursor().fetchall()
-    return hosts
+def getUnbilledHosts():
+    """
+    Find hosts that were executed against that do not show up
+    in the current billing cycle as having been reported.
+    """
+    # TODO Add billing period logic
+    executed_hosts = {x.host_name: x.modified for x in JobHostSummary.objects.order_by('host_name', '-modified').distinct('host_name')}
+    billed_hosts = {x.host_name: x.billed_date for x in BilledHost.objects.all()}
+    new_hosts = []
+    for host_name in executed_hosts.keys():
+        if host_name not in billed_hosts.keys():
+            new_hosts.append(host_name)
+        elif executed_hosts[host_name] > billed_hosts[host_name]:
+            # Seen it, but executed again since last report
+            # Keep track of this or don't care?
+            pass
+        else:
+            # Do nothing, already reported
+            pass
+    return new_hosts
 
-def markHostsBilled(hosts):
-    query = "UPDATE unique_host SET billed_date = current_timestamp WHERE host_name = '%s'"
-    for host in hosts:
-        (name, _, _) = host
-        getCursor().execute(query % name)
-    conn.commit()
+
+def markHostsBilled(unbilled_hosts):
+    """
+    Add/update reported hosts in the DB
+    """
+    for host in unbilled_hosts:
+        b = BilledHost(host_name = host, billed_date = models.functions.Now())
+        # TODO b.save()
+
+
+def recordBillingInstance(billing_data):
+    """
+    Store a report of the successful billing
+    """
+    b = BillingRecord(hosts=",".join(billing_data['hosts']),
+                      billed_date = models.functions.Now(),
+                      dimension = billing_data['dimension'],
+                      quantity = len(billing_data['hosts']),
+                      managed_app_id = billing_data['managed_app_id'],
+                      resource_id = billing_data['resource_id'],
+                      plan = billing_data['plan'],
+                      usage_event_id = billing_data['usage_event_id'])
+    b.save()
